ac494a65e361cd0395bc9c2cbbc5189d2ca1fb11
charroux <benoit.charroux@free.fr>
2017-11-29 15:22:35 +0100

    tp4

---
+++ src/tp4/Zoo.java
@@ -0,0 +1,12 @@
+package tp4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zoo {
+	
+	// private List<Animal> animaux = new List<Animal>(); ne compile pas (on ne peut pas instancier une interface
+	// private ArrayList<Animal> animaux = new ArrayList<Animal>(); // possible
+	private List<Animal> listeAnimaux = new ArrayList<Animal>(); // mieux
+
+}
