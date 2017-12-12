cc4bcb1a8b70ab883aa0faf5ab79abfff2af2dd2
charroux <benoit.charroux@free.fr>
2017-12-12 11:25:29 +0100

    gestion exception

--- src/tp7/GestionFichier.java
+++ src/tp7/GestionFichier.java
@@ -2,6 +2,7 @@ package tp7;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.net.URI;
 import java.util.Scanner;
 
@@ -9,42 +10,60 @@ public class GestionFichier {
 	
 	public static void main(String[] a) {
 
+		// acces fichier chemin absolu
+		
+		URI uri = null;
+		Scanner scanner = null;
+		
 		try {
-			String user = System.getProperty("user.home");
+			String user = System.getProperty("user.home");	// demande au système d'exploitation
 			System.out.println(user);
-			user = user.replace("\\", "/");
+			user = user.replace("\\", "/");					// uniquement sous Windows, sinon ne fait rien
 			System.out.println(user);
-			String location = "file:///" + user + "/bidon.txt";
-			URI uri = new URI(location);
+			String location = "file:///" + user + "/bidon.txt";	// création URI
+			uri = new URI(location);
 			System.out.println(uri);
 			File file = new File(uri);
 			FileInputStream fis = new FileInputStream(file);
-			Scanner scanner = new Scanner(fis);
+			scanner = new Scanner(fis);
 			String s;
 			while(scanner.hasNextLine()) {
 				s = scanner.nextLine();
 				System.out.println(s);
 			}
-		} catch(Exception e) {
+		} catch(FileNotFoundException e) {
+			System.out.println("Pas de fichier dans " + uri);
+		} catch(Exception e){
 			e.printStackTrace();
+		} finally {						// on passe dans la clause finally qu'il y eu un exception ou pas
+			if(scanner != null) {
+				scanner.close();		// on ferme le fichier qui est détenu par le système d'exploitation
+			}
 		}
-		/*try {
-			URI uri = new URI("file:///c:/foo/bar/bidon.txt");	// toujours / => portable entre Linux et Windows
-			File file = new File(uri);
-			FileInputStream fis = new FileInputStream(file);
-			Scanner scanner = new Scanner(fis);
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	
+		// acces fichier chemin relatif
+		String path = null;
 		
 		try {
-			String path = "." + File.separator + "data" + File.separator + "bidon1.txt";
+			path = "." + File.separator + "data" + File.separator + "bidon1.txt";
 			File file = new File(path);
 			FileInputStream fis = new FileInputStream(file);
-			Scanner scanner = new Scanner(fis);
+			scanner = new Scanner(fis);
+			String s;
+			while(scanner.hasNextLine()) {
+				s = scanner.nextLine();
+				System.out.println(s);
+			}
+			scanner.close();
+		} catch(FileNotFoundException e) {
+			System.out.println("Pas de fichier dans " + path);
 		} catch(Exception e) {
 			e.printStackTrace();
-		}*/
+		}  finally {						// on passe dans la clause finally qu'il y eu un exception ou pas
+			if(scanner != null) {
+				scanner.close();		// on ferme le fichier qui est détenu par le système d'exploitation
+			}
+		}
 	}
 
 }
